bbf1fd55a6f1a87e02fa4f526eed46e26596b362
Pito Salas <pitosalas@gmail.com>
2022-04-03 20:51:21 -0400

    tiny

--- src/btoutback1.py
+++ src/btoutback1.py
@@ -2,5 +2,12 @@ import py_trees
 import random
 import math
 import time
-from py_trees import Blackboard
-import bt_utils
+
+from py_trees import composites
+
+
+class BtTest():
+  def create_tree(self):
+    start_turn = 
+    turn_tree = py_trees.composites.composites.Selector(children=start_turn, turn, end_turn)
+
