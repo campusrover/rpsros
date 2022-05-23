3783ba6639c783ee8b01f42a43bd0b8bdb388d50
Pito Salas <pitosalas@gmail.com>
2022-05-23 20:24:33 +0000

    trying bullet_pole on real robot

--- src/pid.py
+++ src/pid.py
@@ -1,3 +1,4 @@
+import numpy as np
 class PID():
     def __init__(self, min_val, max_val, kp, ki, kd):
         self.min_val = min_val
@@ -8,9 +9,6 @@ class PID():
         self.integral = 0
         self.prev_error = 0
 
-    def constrain(self, val):
-        return min(self.max_val, max(self.min_val, val))
-
     def compute(self, setpoint, measured_value):
         error = setpoint - measured_value
         self.integral += error
@@ -18,4 +16,6 @@ class PID():
         if setpoint == 0 and self.prev_error == 0: self.integral = 0
         pid = (self.kp * error + self.ki * self.integral + self.kd * self.derivative)
         self.prev_error = error
-        return self.constrain(pid)
+        r = np.clip(pid, self.min_val, self.max_val)
+        # print(f"** PID {pid:1.2} {error:1.2} {r:1.2}")
+        return np.clip(pid, self.min_val, self.max_val)
