1109fd02562445c0c54c2408ed20d208bb7e0da1
Pito Salas <pitosalas@gmail.com>
2022-07-25 03:19:28 +0000

    more work

--- src/bru_utils.py
+++ src/bru_utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 
 import rospy
 from scipy.spatial import distance
@@ -9,6 +9,7 @@ from math import pi, sqrt, atan2, isclose
 # Stand alone functions
 
 # Simple trig
+# angles are in radians, -pi < angle < pi. 0 is at 3 o'clock
 
 def calc_distance(pa: Pose2D, pb: Pose2D) -> float:
     return distance.euclidean([pa.x, pa.y], [pb.x, pb.y])
@@ -17,7 +18,10 @@ def invalid_pose(pose: Pose2D) -> bool:
     return pose.x == 0 and pose.y == 0 and pose.theta == 0
 
 def invert_angle(angle: float) -> float:
-    return (angle + pi) % (2 * pi)
+    return (angle + pi) % (pi)
+
+def offset_point(point: Pose2D, distance: float) -> Pose2D:
+    pass
 
 # Logging and   debugging  functions
 def info(msg: str):
@@ -41,6 +45,6 @@ class PID():
         if setpoint == 0 and self.prev_error == 0: self.integral = 0
         pid = (self.kp * error + self.ki * self.integral + self.kd * self.derivative)
         self.prev_error = error
-        r = np.clip(pid, self.min_val, self.max_val)
+        # r = np.clip(pid, self.min_val, self.max_val)
         # print(f"** PID {pid:1.2} {error:1.2} {r:1.2}")
         return np.clip(pid, self.min_val, self.max_val)
