8df3713bc0668efa1f29c562e8b81630cc7378a2
Pito Salas <pitosalas@gmail.com>
2023-07-10 16:14:58 +0000

    wip

--- src/rgparser2.py
+++ src/rgparser2.py
@@ -5,15 +5,17 @@ Package of very simple parsing used in Robogym and friends
 from json import JSONDecodeError, loads
 from prompt_toolkit import PromptSession
 
-class Parser:
 
-    """General very simple parser for commands to rgserver"""
-    def __init__(self, initial_variables, command_table):
+class Parser:
+    """General purpose simple interactive command processor"""
+
+    def __init__(self, symbol_table, command_table):
         # Initialize an empty dictionary to store variables and their values
-        self.variables = initial_variables
         self.session = None
         self.command_info = None
+        self.symbol_table = symbol_table
         self.session = PromptSession()
+        self.command_input = None
 
         # Define the built-in command table
         self.local_command_table = {
@@ -28,13 +30,15 @@ class Parser:
                 "nargs": 2,
                 "description": "Set a variable to a value",
                 "handler": self.set_variable,
-                "usage": "Invalid command. Usage: set <varname> <value>"
+                "usage": "Invalid command. Usage: set <varname> <value>",
             },
             "show": {
                 "nargs": 0,
                 "args": ["[<variable>]"],
                 "description": "Show all variables or the value of a specific variable",
                 "handler": self.show_variable,
+                "usage": "Invalid command. Usage: show <varname>",
+
             },
             "reset": {
                 "args": [],
@@ -43,47 +47,55 @@ class Parser:
                 "handler": self.reset,
             },
         }
-        self.command_table = { **self.local_command_table, **command_table}  
+        self.command_table = {**self.local_command_table, **command_table}
 
     def print_commands(self, _):
         """Display all the commands we know about"""
-        print("\n".join([f"- {command} {' '.join(info['args'])}: {info['description']}" for command, info in self.command_table.items()]))
+        print(
+            "\n".join(
+                [
+                    f"- {command} {' '.join(info['args'])}: {info['description']}"
+                    for command, info in self.command_table.items()
+                ]
+            )
+        )
 
-
-    def reset(self, args):
+    def reset(self, _):
         """Reset the state of the commands"""
-        self.variables.clear()
+        self.symbol_table.clear()
         print("State has been reset.")
 
     def set_variable(self, args):
         """Set a variable to a value. Legal values are words and numbers"""
-        parse_args = self.get_tokens(args)
-        if not parse_args:
+        if not args:
             print(self.command_info["usage"])
             return False
         else:
-            self.variables[parse_args[0]] = parse_args[1]
-            print(f"Variable '{parse_args[0]}' has been set to '{parse_args[1]}'.")
+            self.symbol_table[args[0]] = args[1]
+            print(f"Variable '{args[0]}' has been set to '{args[1]}'.")
             return True
 
     def show_variable(self, args):
         """Show the value of a variable"""
-        parse_args = self.get_tokens(args)
-        if len(parse_args) == 0 and len(self.variables) > 0:
-            print("\n".join([f"{name}: {value}" for name, value in self.variables.items()]))
-            return True
-        if len(self.variables) == 0:
-            print("No variables have been set.")
-            return False
-        if len(parse_args) == 1:
-            variable_name = parse_args[0]
-            if variable_name in self.variables:
-                print(f"The value of '{variable_name}' is '{self.variables[variable_name]}'.")
-                return True
-            print(f"Variable '{variable_name}' has not been set.")
-            return False
-        print(self.command_info["usage"], "set_variable")
-        return False
+        if len(args) == 0 and len(self.symbol_table) > 0:
+            self.show_all_variables()
+        elif len(args) == 1:
+            print(f" {self.command_input[1]} -> {args[0]}")
+        else:
+            print(self.command_info["usage"], "set_variable")
+
+    def get_variable(self, name):
+        if len(self.symbol_table) == 0:
+            raise ValueError("No variables have been set")
+        result = self.symbol_table.get(name, False)
+        if result is False:
+            raise ValueError("Unknown variable")
+        return result
+
+    def show_all_variables(self):
+        print(
+            "\n".join([f"{name} -> {value}" for name, value in self.symbol_table.items()])
+        )
 
     def get_tokens(self, val_list):
         """parse a list of values, or return False if there's a parse error"""
@@ -100,14 +112,17 @@ class Parser:
         """Parse a value from a token. Either a word or a float or a list"""
         # Check if the token is a variable name, and if so, convert it to its digital value
         if token.isidentifier():
-            return token
+            value = self.symbol_table.get(token)
+            if not value:
+                value = token
+            return value
         try:
             return float(token)
         except ValueError:
             pass
         # if not, see if works as a json value
         try:
-            return (loads(token))
+            return loads(token)
         except JSONDecodeError:
             print(f"bad json: {token}")
             return False
@@ -118,16 +133,15 @@ class Parser:
         while True:
             try:
                 raw_input = self.session.prompt(">>> ")
-                command_input = raw_input.strip().split()
-                if len(command_input) > 0:
-                    command = command_input[0]
-                    args = command_input[1:]
+                self.command_input = raw_input.strip().split()
+                if len(self.command_input) > 0:
+                    command = self.command_input[0]
+                    args = self.get_tokens(self.command_input[1:])
                     return command, args
-                else:
-                    return None, None
+                return None, None
             except KeyboardInterrupt:
                 continue
-        
+
     def cli(self):
         """Main entry point to parse commands"""
         while True:
