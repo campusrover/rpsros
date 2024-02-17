b9b729ae8362b3958cad68ec37c029ce3a607e04
Pito Salas <pitosalas@gmail.com>
2024-02-17 23:02:20 +0000

    beginning

--- sodacan/src/object_find.py
+++ sodacan/src/object_find.py
@@ -8,8 +8,6 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
-bridge = CvBridge()
-
 def image_callback(msg):
     bridge = CvBridge()
     try:
@@ -17,15 +15,41 @@ def image_callback(msg):
     except CvBridgeError as e:
         print(e)
     else:
-        # Process cv_image here
-        cv2.imshow("Camera Image", cv_image)
-        # cv2.waitKey(3)
-        # ros_image = bridge.cv2_to_imgmsg(cv_image, "bgr8")
-        # # Publish the image
-        # image_pub.publish(ros_image)
+        arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+        arucoParams = cv2.aruco.DetectorParameters_create()
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(cv_image, arucoDict, parameters=arucoParams)
+        if len(corners) > 0:
+# at least one Aruco detected. Flatten the ArUco IDs list
+            ids = ids.flatten()            
+            # loop over the detected ArUCo corners
+            for (markerCorner, markerID) in zip(corners, ids):
+                # extract the marker corners (which are always returned in top-left, top-right, bottom-right, and bottom-left order)
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+                
+                # draw the bounding box of the ArUCo detection
+                cv2.line(cv_image, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(cv_image, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(cv_image, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(cv_image, bottomLeft, topLeft, (0, 255, 0), 2)
+                # compute and draw the center (x, y)-coordinates of the ArUco marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(cv_image, (cX, cY), 4, (0, 0, 255), -1)
+                # draw the ArUco marker ID on the image
+                cv2.putText(cv_image, str(markerID),
+                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
+            ros_image = bridge.cv2_to_imgmsg(cv_image, "bgr8")
+            image_pub.publish(ros_image)
 
 if __name__ == '__main__':
     rospy.init_node('object_find', anonymous=True)
     rospy.Subscriber("/cv_camera/image_raw", Image, image_callback)
-    image_pub = rospy.Publisher('/camera/image_raw', Image, queue_size=10)
+    image_pub = rospy.Publisher('/sodacan/image_raw', Image, queue_size=10)
     rospy.spin()
