948bf449fb68c070b8e550ecfc6fe18a0e4cfc2c
Pito Salas <pitosalas@gmail.com>
2024-02-20 15:13:21 +0000

    small change

--- sodacan/src/detector.py
+++ sodacan/src/detector.py
@@ -5,14 +5,15 @@ import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import Float64MultiArray
+
 import numpy as np
 
 DEBUG = True
 
-
 class Detector:
     def __init__(self, marker_size: float):
-        rospy.init_node("aruco_marker_detector")
+        self.detector_node = rospy.init_node("aruco_marker_detector")
         self.bridge = CvBridge()
         self.marker_size = marker_size
         self.dictionary = cv2.aruco.getPredefinedDictionary(
@@ -21,6 +22,8 @@ class Detector:
         self.camera_matrix = None
         self.dist_coeffs = None
         self.camera_info_needed = True
+        self.float_array_msg = Float64MultiArray()
+
 
         if DEBUG:
             self.image_pub = rospy.Publisher("/sodacan/image_raw",
@@ -29,7 +32,7 @@ class Detector:
 
     def image_callback(self, msg: Image):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        corners, ids, rejected = cv2.aruco.detectMarkers(
+        corners, ids, _ = cv2.aruco.detectMarkers(
             cv_image, self.dictionary, parameters=self.parameters)
         # If markers are detected
         if ids is not None:
@@ -41,8 +44,9 @@ class Detector:
                 bearing = np.arctan2(
                     tvec[0][0],
                     tvec[0][2])  # Convert from radians to degrees if necessary
+                if DEBUG:
+                    self.detector_node.loginfo(f"Distance: {distance:.2f}, Bearing: {bearing:.2f} radians")
 
-                print(f"Distance: {distance:.2f}, Bearing: {bearing:.2f} radians")
 
         if DEBUG:
             # Draw detected markers on the image
@@ -59,6 +63,8 @@ class Detector:
     def run(self):
         rospy.Subscriber("/cv_camera/image_raw", Image, self.image_callback)
         rospy.Subscriber("/cv_camera/camera_info", CameraInfo, self.camera_info_callback)
+        self.detect = rospy.Publisher('detect', Float64MultiArray, queue_size=10)
+
         rospy.spin()
 
 if __name__ == "__main__":
