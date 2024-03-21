5cae5ddc253cf817f5c3c6ae8e66f5c85d3cec8e
Pito Salas <pitosalas@gmail.com>
2024-03-21 00:46:31 +0000

    work in progress

--- sodacan/src/ai_sample1.py
+++ sodacan/src/ai_sample1.py
@@ -1,36 +1,32 @@
-#!/usr/bin/env python
-
 import rospy
 import tf
+from geometry_msgs.msg import PointStamped
 
 if __name__ == '__main__':
     rospy.init_node('coordinate_converter')
 
+    target = "map"
+    source = "fiducial_0"
     listener = tf.TransformListener()
 
     # Wait for the transformation to become available
-    listener.waitForTransform('/fiducial_0', '/map', rospy.Time(), rospy.Duration(1.0))
+    listener.waitForTransform(target, source, rospy.Time(), rospy.Duration(1.0))
 
     # Specify the time at which you want to look up the transform
     transform_time = rospy.Time(0)
 
-    # Specify the source and target frames
-    source_frame = '/fiducial_0'
-    target_frame = '/map'
-
     try:
-        # Get the transform from source_frame to target_frame
-        (trans, rot) = listener.lookupTransform(target_frame, source_frame, transform_time)
+        # Create a PointStamped message with the source frame and coordinate
+        source_point = PointStamped()
+        source_point.header.frame_id = source
+        source_point.point.x = 1.0
+        source_point.point.y = 2.0
+        source_point.point.z = 3.0
 
-        # Specify the coordinate you want to convert
-        source_point = (1.0, 2.0, 3.0)
-
-        # Apply the transformation to convert the coordinate
-        target_point = listener.transformPoint(target_frame, source_point, transform_time)
+        # Transform the coordinate to the target frame
+        target_point = listener.transformPoint(target, source_point)
 
         rospy.loginfo("Converted coordinate: %s", target_point)
 
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
         rospy.logwarn("Failed to convert coordinate")
-
-    rospy.spin()
--- sodacan/src/aruco_movebase.py
+++ sodacan/src/aruco_movebase.py
@@ -5,8 +5,13 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PointStamped
+from actionlib_msgs.msg import GoalStatusArray
+from tf.transformations import quaternion_from_euler, quaternion_multiply
+from geometry_msgs.msg import Quaternion
+import tf
+import math
 
-import tf2_ros
 
 class ArucoMoveBase():
     def __init__(self):
@@ -14,76 +19,89 @@ class ArucoMoveBase():
         rospy.init_node('arucomovebase')
 
     def set_initial_pose(self):
-
-        # Create a publisher for the initial pose
         initial_pose_pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=10)
-
-        # Create a PoseWithCovarianceStamped message
         initial_pose_msg = PoseWithCovarianceStamped()
-
-        # Set the header frame ID to the map frame
         initial_pose_msg.header.frame_id = 'map'
-
-        # Set the robot's position and orientation
         initial_pose_msg.pose.pose.position.x = 0.0
         initial_pose_msg.pose.pose.position.y = 0.0
         initial_pose_msg.pose.pose.position.z = 0.0
-
         initial_pose_msg.pose.pose.orientation.x = 0.0
         initial_pose_msg.pose.pose.orientation.y = 0.0
         initial_pose_msg.pose.pose.orientation.z = 0.0
         initial_pose_msg.pose.pose.orientation.w = 1.0
-
         # Publish the initial pose
         initial_pose_pub.publish(initial_pose_msg)
 
-    def get_pose_in_tf(self, pose_tf, dest_tf):
-        
+    def get_pose_in_tf(self, source_pose, source_tf, target_tf):
+        listener = tf.TransformListener()
+        listener.waitForTransform(target_tf, source_tf, rospy.Time(), rospy.Duration(1.0))
+        transform_time = rospy.Time(0)
+        try:
+            target_pose = listener.transformPose(target_tf, source_pose)
+            rotation_quaternion = quaternion_from_euler(0, 0, 0)
+            o = target_pose.pose.orientation
+            unrotated_quaternion = [o.x, o.y, o.z, o.w]
+            rotated_quaternion = quaternion_multiply(unrotated_quaternion, rotation_quaternion)
+            target_pose.pose.orientation.x = rotated_quaternion[0]
+            target_pose.pose.orientation.y = rotated_quaternion[1]
+            target_pose.pose.orientation.z = rotated_quaternion[2]
+            target_pose.pose.orientation.w = rotated_quaternion[3]
+            rospy.loginfo("Converted coordinate: %s", target_pose)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn("Failed to convert coordinate")
+        return target_pose
 
-
-    def set_tf_as_navigation_goal(self, from_frame, to_frame):
-        # Create a client for the move_base action
+    def set_pose_as_navigation_goal(self, goal_pose):
         move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         move_base_client.wait_for_server()
 
-        # Create a TF buffer and listener
-        tf_buffer = tf2_ros.Buffer()
-        tf_listener = tf2_ros.TransformListener(tf_buffer)
-
-        # Get the transform from from_frame to to_frame
-        try:
-            transform = tf_buffer.lookup_transform(from_frame, to_frame, rospy.Time.now(), rospy.Duration(1.0))
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            rospy.logwarn(f"Cannot get transform {from_frame}' to {to_frame} (error: {str(e)})")
-            return
-
-        # Create a PoseStamped message with the transform
-        goal_pose = PoseStamped()
-        goal_pose.header = transform.header
-        goal_pose.pose.position = transform.transform.translation
-        goal_pose.pose.orientation = transform.transform.rotation
-
-        # Transform the goal pose from 'tfname' frame to 'map' frame
-        try:
-            tf_buffer.transform(goal_pose, 'map', rospy.Duration(1.0))
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rospy.logwarn(f"Cannot get transform {from_frame}' to {to_frame} (error: {str(e)})")
-            return
-
-        # Create a move_base goal with the transformed goal pose
         goal = MoveBaseGoal()
         goal.target_pose = goal_pose
 
-        # Send the goal to the move_base action
         move_base_client.send_goal(goal)
         move_base_client.wait_for_result()
-
+        print(move_base_client.get_state())
         if move_base_client.get_state() == actionlib.GoalStatus.SUCCEEDED:
             rospy.loginfo("Navigation goal set successfully!")
         else:
             rospy.logwarn("Failed to set navigation goal.")
 
+    def set_pose_as_navigation_goal1(self, goal_pose):
+        goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+        goal_pub.publish(goal_pose)
+
+
+    def check_navigation_status(self):    
+        status_sub = rospy.Subscriber('/move_base/status', GoalStatusArray, self.callback)
+        rospy.spin()
+
+    def callback(self,status_msg):
+        if len(status_msg.status_list) > 0:
+            status = status_msg.status_list[0].status
+            if status == 1:  # Goal is currently being processed
+                rospy.loginfo("Robot is currently moving to the goal.")
+            elif status == 3:  # Goal has been reached
+                rospy.loginfo("Robot has reached the goal.")
+            elif status == 4:  # Goal was aborted
+                rospy.logwarn("Robot failed to reach the goal.")
+            elif status == 5:  # Goal was rejected
+                rospy.logwarn("Robot goal was rejected.")
+
+
 if __name__ == '__main__':
     an = ArucoMoveBase()
     an.set_initial_pose()
-    an.set_tf_as_navigation_goal("fiducial_0")
+
+    source_tf = "fiducial_0"
+    target_tf = "map"
+    source_pose = PoseStamped()
+    source_pose.header.frame_id = source_tf
+    source_pose.pose.position.x = 0.0
+    source_pose.pose.position.y = 0.0
+    source_pose.pose.position.z = 0.0
+    q = quaternion_from_euler(0, 0, 0)
+    source_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
+    goal_pose = an.get_pose_in_tf(source_pose, source_tf, target_tf)
+    an.set_pose_as_navigation_goal(goal_pose)
+    an.check_navigation_status()
+
