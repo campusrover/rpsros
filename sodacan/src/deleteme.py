060a7c3c4058cb99a7f704f232bd9a8b3071602e
Pito Salas <pitosalas@users.noreply.github.com>
2024-05-20 16:28:41 -0400

    update

--- sodacan/src/deleteme.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-try:
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_MIP_36h12)
-    print("DICT_ARUCO_MIP_36h12 is supported.")
-except AttributeError as e:
-    print("DICT_ARUCO_MIP_36h12 is not supported:", str(e))
